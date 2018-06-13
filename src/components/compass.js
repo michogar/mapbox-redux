4b1725403d8f71c15135bb72ccf047c22a8d1acb
michogar <michogar@gmail.com>
2018-06-13 13:40:00 +0200

    Added set bearing to 0 on bearing clicks

--- src/components/compass.js
+++ src/components/compass.js
@@ -1,16 +1,21 @@
 class Compass {
   constructor (parent, control, store) {
     this.store = store
+    const { jumpTo } = control.MapActionCreators
+    this.jumpTo = jumpTo.bind(this)
     const { bearing } = this.store.getState()
     this.bearing = bearing || 0
     this.parent = document.getElementById(parent)
-    this.unsubscriber = store.subscribe(this.onChangeDirection.bind(this))
+    this.unsubscriber = store.subscribe(this.onChangeBearing.bind(this))
     this.render()
   }
-  onChangeDirection () {
+  onChangeBearing () {
     const { bearing } = this.store.getState()
     this.bearing = bearing
     this.render()
+  }
+  onClickBearing () {
+    this.store.dispatch(this.jumpTo({ bearing: 0 }))
   }
   render () {
     let compass = document.getElementById('compass')
@@ -24,6 +29,7 @@ class Compass {
     compassButton.className = 'button icon compass'
     compassButton.style.transformOrigin = 'center'
     compassButton.style.transform = `rotate(${-this.bearing}deg)`
+    compassButton.addEventListener('click', this.onClickBearing.bind(this))
 
     compass.appendChild(compassButton)
     this.parent.appendChild(compass)
